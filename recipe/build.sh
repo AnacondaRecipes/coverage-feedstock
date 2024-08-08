67b004e616bcf6e056f67fe796086ea7e5a92b38
Denis Dupeyron <ddupeyron@anaconda.com>
2024-08-08 16:21:21 -0700

    recipe/build.sh: fix versioning due to free-threading ABI

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,6 @@ $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 PY_VER_MAJ=$($PYTHON -c "import os; print('_'.join(os.environ['PY_VER'].split('.')[0]))")
 
 rm "${PREFIX}/bin/coverage${PY_VER_MAJ}"
-rm "${PREFIX}/bin/coverage-${PY_VER}"
+rm "${PREFIX}/bin/coverage-${PY_VER% *}"
 
 ls $PREFIX/bin/coverage*
